3169ea84f758e1152537b5814b01345ecad872de
“Mohammad <w0848655@myscc.ca>
2024-09-19 16:22:32 -0400

    added any array with 6 instances for my object, and displayed info using structural directives.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MacBook} from "./models/mac-book";
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,20 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'MohammadKansoLearningAngular';
-  name = 'Mohammad Kanso';
-  program = 'Computer Programming';
+// export class AppComponent {
+//   title = 'MohammadKansoLearningAngular';
+//   name = 'Mohammad Kanso';
+//   program = 'Computer Programming';
+//
+// }
 
+export class AppComponent {
+  macBook1: MacBook = {model: 'MacBook Pro 14-inch 2021', chip: 'M1', serialNumber: '11A11', memory: 16, color: 'Silver', hasOwner: true };
+  macBook2: MacBook = {model: 'MacBook Pro 16-inch 2023', chip: 'M2', serialNumber: '11A22', memory: 32, color: 'Space Grey', hasOwner: false };
+  macBook3: MacBook = {model: 'MacBook Air 15-inch 2023', chip: 'M2', serialNumber: '11A33', memory: 16, color: 'Silver', hasOwner: false };
+  macBook4: MacBook = {model: 'MacBook Air 13-inch 2024', chip: 'M3', serialNumber: '11A44', memory: 16, color: 'Starlight', hasOwner: true };
+  macBook5: MacBook = {model: 'MacBook Pro 16-inch 2023', chip: 'M3', serialNumber: '11A55', memory: 32, color: 'Silver', hasOwner: true };
+  macBook6: MacBook = {model: 'MacBook Pro 14-inch 2021', chip: 'M3', serialNumber: '11A66', memory: 32, color: 'Space Grey', hasOwner: false };
+
+  macBookList: MacBook[] = [this.macBook1, this.macBook2, this.macBook3, this.macBook4, this.macBook5, this.macBook6]
 }
