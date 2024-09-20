3dae1ad113291350b9fc4720cd4db06ac570bff1
“Mohammad <w0848655@myscc.ca>
2024-09-19 20:36:48 -0400

    UPDATED: added any array with 6 instances for my object, and displayed info using structural directives.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MacBook} from "./models/mac-book";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
