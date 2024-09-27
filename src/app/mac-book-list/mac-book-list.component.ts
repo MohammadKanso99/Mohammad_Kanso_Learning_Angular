506105cb6ad69cc948016a997de2b011a01dcc28
“Mohammad <w0848655@myscc.ca>
2024-09-27 10:28:40 -0400

    added images with styling to my html list file. added the references of images as comments in my list-item.html file

--- src/app/mac-book-list/mac-book-list.component.ts
+++ src/app/mac-book-list/mac-book-list.component.ts
@@ -16,12 +16,12 @@ import {NgForOf} from "@angular/common";
 
 // part 6
 export class MacBookListComponent {
-  macBook1: MacBook = {model: 'MacBook Pro 14-inch 2021', chip: 'M1', serialNumber: '11A11', memory: 16, color: 'Silver', hasOwner: true };
-  macBook2: MacBook = {model: 'MacBook Pro 16-inch 2023', chip: 'M2', serialNumber: '11A22', memory: 32, color: 'Space Grey', hasOwner: false };
-  macBook3: MacBook = {model: 'MacBook Air 15-inch 2023', chip: 'M2', serialNumber: '11A33', memory: 16, color: 'Silver', hasOwner: false };
-  macBook4: MacBook = {model: 'MacBook Air 13-inch 2024', chip: 'M3', serialNumber: '11A44', memory: 16, color: 'Starlight', hasOwner: true };
-  macBook5: MacBook = {model: 'MacBook Pro 16-inch 2023', chip: 'M3', serialNumber: '11A55', memory: 32, color: 'Silver', hasOwner: true };
-  macBook6: MacBook = {model: 'MacBook Pro 14-inch 2021', chip: 'M3', serialNumber: '11A66', memory: 32, color: 'Space Grey', hasOwner: false };
+  macBook1: MacBook = {model: 'MacBook Pro 14-inch 2021', chip: 'M1', serialNumber: 'macbook1', memory: 16, color: 'Silver', hasOwner: true };
+  macBook2: MacBook = {model: 'MacBook Pro 16-inch 2023', chip: 'M2', serialNumber: 'macbook2', memory: 32, color: 'Space Grey', hasOwner: false };
+  macBook3: MacBook = {model: 'MacBook Air 15-inch 2023', chip: 'M2', serialNumber: 'macbook3', memory: 16, color: 'Silver', hasOwner: false };
+  macBook4: MacBook = {model: 'MacBook Air 13-inch 2024', chip: 'M3', serialNumber: 'macbook4', memory: 16, color: 'Starlight', hasOwner: true };
+  macBook5: MacBook = {model: 'MacBook Pro 16-inch 2023', chip: 'M3', serialNumber: 'macbook5', memory: 32, color: 'Silver', hasOwner: true };
+  macBook6: MacBook = {model: 'MacBook Pro 14-inch 2021', chip: 'M3', serialNumber: 'macbook6', memory: 32, color: 'Space Grey', hasOwner: false };
 
   macBookList: MacBook[] = [this.macBook1, this.macBook2, this.macBook3, this.macBook4, this.macBook5, this.macBook6]
 }
