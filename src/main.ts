b0f8462a8af5782aae1d165aa397960de23ef711
“Mohammad <w0848655@myscc.ca>
2024-10-08 15:38:58 -0400

    router correctly set up in main.ts list of routes created

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {macbookList} from "./app/Shared/mockMacBook.data";
+import {MacBookListComponent} from "./app/mac-book-list/mac-book-list.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+const routes: Routes = [
+  {path:'', redirectTo: '/macbooks', pathMatch: 'full'},
+  { path: 'macbooks', component: MacBookListComponent}];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(() => console.log('Bootstrap successful'));
