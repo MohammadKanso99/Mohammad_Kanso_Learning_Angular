bb25a4e9e509f9781c21703d7bc23cd335e03881
“Mohammad <w0848655@myscc.ca>
2024-11-07 23:06:10 -0500

    * Updated data.service.ts to manage all CRUD operations using HTTP requests instead of local data

--- src/app/services/macbooks.service.ts
+++ src/app/services/macbooks.service.ts
@@ -2,45 +2,40 @@ import { Injectable } from '@angular/core';
 import {MacBook} from "../Shared/models/mac-book";
 import {Observable, of} from "rxjs";
 import {macbookList} from "../Shared/mockMacBook.data";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MacbooksService {
+  private apiUrl = 'api/macbooks'; //url to web api
   private macbooks: MacBook[] = macbookList;//Local copy of student data for CRUD Operations
-  constructor() {
-  }
+  constructor(private http: HttpClient) { }//DI http
 
   // step 6
   getMacbook(): Observable<MacBook[]> {
-    return of(macbookList); //Return and observable that emits mock macbook data
+    return this.http.get<MacBook[]>(this.apiUrl);
   }
 
   // step 9a
-  getMacbookById(macbookId: number): Observable<MacBook | undefined> {
-    const macbook = this.macbooks.find(macbook => macbook.id === macbookId);
-    return of(macbook);
+  getMacbookById(id: number): Observable<MacBook> {
+    return this.http.get<MacBook>(`${this.apiUrl}/${id}`); //return a single student
   }
 
-  // add macbook 9b
-  addMacbook(newMacBook: MacBook): Observable<MacBook[]> {
-    this.macbooks.push(newMacBook)
-    return of(this.macbooks);
+  addMacbook(macbook: MacBook): Observable<MacBook> {
+    macbook.id = this.generateNewId();
+    return this.http.post<MacBook>(this.apiUrl, macbook);
   }
 
-  // update macbook 9c
-  updateMacbook(updatedMacBook: MacBook): Observable<MacBook[]> {
-    const index = this.macbooks.findIndex(macbook => macbook.id === updatedMacBook.id);
-    if (index !== -1) {
-      this.macbooks[index] = updatedMacBook;
-    }
-    return of(this.macbooks);
+  updateMacbook(macbook: MacBook): Observable<MacBook | undefined> {
+    const url = `${this.apiUrl}/${macbook.id}`;
+    return this.http.put<MacBook>(url, macbook);
   }
 
-  // delete macbook 9d
-  deleteMacbook(macbookId: number): void {
-    this.macbooks = this.macbooks.filter(macbook => macbook.id !== macbookId);
-   }
+  deleteMacbook(id: number): Observable<{}> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete(url);
+  }
   // New method to generate a new unique ID
   generateNewId(): number {
     return this.macbooks.length > 0 ? Math.max(...this.macbooks.map(macbook => macbook.id)) + 1 : 1;
