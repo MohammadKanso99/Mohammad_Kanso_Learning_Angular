ab69ca041d3bd37ecbd5742801f332f8ae8a08b9
“Mohammad <w0848655@myscc.ca>
2024-11-10 20:50:22 -0500

    * I tried few changes to te the code

--- src/app/Shared/mockMacBook.data.ts
+++ src/app/Shared/mockMacBook.data.ts
@@ -13,5 +13,6 @@ export const macbookList: MacBook[] = [
  {id: 4,model: 'MacBook Air 13-inch 2024', chip: 'M3', serialNumber: 'macbook4', memory: 16, color: 'Starlight', hasOwner: true },
 {id: 5, model: 'MacBook Pro 16-inch 2023', chip: 'M3', serialNumber: 'macbook5', memory: 32, color: 'Silver', hasOwner: true },
 {id: 6, model: 'MacBook Pro 14-inch 2021', chip: 'M3', serialNumber: 'macbook6', memory: 32, color: 'Space Grey', hasOwner: false }
+
   //Add more as needed
 ];
